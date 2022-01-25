be23834bcb9701161d59b36aa9476e7790d50437
jetspeedy18 <61331514+jetspeedy18@users.noreply.github.com>
2022-01-24 22:49:12 -0600

    d

--- asdfghjkl/asdfghjkl/core/engine/ItemHandler.java
+++ asdfghjkl/asdfghjkl/core/engine/ItemHandler.java
@@ -66,7 +66,7 @@ public class ItemHandler {
 		}
 		program.setUniform("trans", player.getPosMat());
 		player.getMesh().render();
-		
+				
 		if (player.hasShot()) {
 			program.setUniform("trans", b.getPosMat());
 			b.getMesh().render();
