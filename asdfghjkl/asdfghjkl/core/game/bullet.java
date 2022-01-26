6a3a309509e26a8edaf44f56a7e426458018a131
jetspeedy18 <61331514+jetspeedy18@users.noreply.github.com>
2022-01-25 21:54:10 -0600

    sd

--- asdfghjkl/asdfghjkl/core/game/bullet.java
+++ asdfghjkl/asdfghjkl/core/game/bullet.java
@@ -48,6 +48,8 @@ public class bullet extends MovableEntity {
 		this.x = x;
 		this.y = y;
 		this.dir=p.getDir();
+		inMotion = true;
+		done = false;
 	}
 
 	public boolean tick(List<GameItem> items){
@@ -103,8 +105,7 @@ public class bullet extends MovableEntity {
 		double ly = y;
 		super.mapClamp(map);
 		
-		if(lx != x || ly != y) inBounds = false;
-		else inBounds = true;
+		if(lx != x || ly != y) done = true;
 	}
 	public boolean inBounds() {
 		return this.inBounds;
@@ -116,9 +117,9 @@ public class bullet extends MovableEntity {
 	}
 
 	public boolean getInMotion() {
-		return this.inMotion;
+		return inMotion;
 	}
 	public void setInMotion(boolean x) {
-		this.inMotion = x;
+		inMotion = x;
 	}
 }
